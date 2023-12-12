374b1f4a9f58ffcd5e9bc43b96cfde6d255c46f7
aulafirdaus <aulafirdaus2945@gmail.com>
2023-12-12 14:05:37 +0700

    menambahkan label

---
+++ app/Filament/Resources/CountryResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CountryResource\Pages;
+use App\Filament\Resources\CountryResource\RelationManagers;
+use App\Models\Country;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CountryResource extends Resource
+{
+    protected static ?string $model = Country::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-flag'; //untuk mengedit icon cukup ganti "flag" dengan yang ada pada heroicon
+    protected static ?string $navigationLabel = 'Country'; //untuk mengedit nama label yang di sebelah kiri
+    protected static ?string $modelLabel = 'Employees Country'; //untuk mengedit tulisan directori path. contoh "country > create"
+    protected static ?string $navigationGroup = 'System Management'; //untuk membuat group label di sebelah kiri
+    protected static ?string $slug = 'employees-countries'; //untuk membuat slug 
+    protected static ?int $navigationSort = 1; //untuk memberikan urutan pada navigasi sebelah kiri
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCountries::route('/'),
+            'create' => Pages\CreateCountry::route('/create'),
+            'edit' => Pages\EditCountry::route('/{record}/edit'),
+        ];
+    }
+}
